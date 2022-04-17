264196f776e17edd0c2eebc6e58c5ac15247d603
Maksimgrom <hjfhjfhjf12>
2022-04-17 19:08:53 +0300

    create database and logger

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO regions(name, short_name)
+VALUES ('Belgorod', 'BEL');
+INSERT INTO regions(name, short_name)
+VALUES ('Bryansk', 'BRY');
+INSERT INTO regions(name, short_name)
+VALUES ('Vladimir', 'VLA');
+INSERT INTO regions(name, short_name)
+VALUES ('Voronezh', 'VOR');
+INSERT INTO regions(name, short_name)
+VALUES ('Ivanovo', 'IVA');
+INSERT INTO regions(name, short_name)
+VALUES ('Kaluga', 'KAL');
+INSERT INTO regions(name, short_name)
+VALUES ('Kostroma', 'KOS');
+INSERT INTO regions(name, short_name)
+VALUES ('Kursk', 'KUR');
+INSERT INTO regions(name, short_name)
+VALUES ('Lipets', 'LIP');
+INSERT INTO regions(name, short_name)
+VALUES ('Moscow ', 'MOS');
+INSERT INTO regions(name, short_name)
+VALUES ('Oryol', 'ORY');
+INSERT INTO regions(name, short_name)
+VALUES ('Ryazan', 'RYA');
+INSERT INTO regions(name, short_name)
+VALUES ('Smolensk', 'SMO');
+INSERT INTO regions(name, short_name)
+VALUES ('Tambov', 'TAM');
+INSERT INTO regions(name, short_name)
+VALUES ('Tver', 'TVE');
+INSERT INTO regions(name, short_name)
+VALUES ('Tula', 'TUL');
