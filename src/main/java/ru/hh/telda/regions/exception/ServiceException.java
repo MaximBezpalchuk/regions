407e6044ee5513a41280ff36821043866f54d22a
Maksimgrom <hjfhjfhjf12>
2022-05-06 23:03:46 +0300

    change exception layer for graphql

--- src/main/java/ru/hh/telda/regions/exception/ServiceException.java
+++ src/main/java/ru/hh/telda/regions/exception/ServiceException.java
@@ -1,11 +1,27 @@
 package ru.hh.telda.regions.exception;
 
-public class ServiceException extends RuntimeException {
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.List;
+
+public class ServiceException extends RuntimeException implements GraphQLError {
 
     private static final long serialVersionUID = 1061524633610399643L;
 
     public ServiceException(String errorMessage) {
         super(errorMessage);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return null;
     }
 
 }
